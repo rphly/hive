1c8f4ebf5f61de78b36a0d56601afbc42456bbdd
chris-lye <christopher.zijian@gmail.com>
2022-03-09 00:04:44 +0800

    Added Navigation drawer

---
+++ app/src/main/java/com/example/hive/FragmentNavigation.java
@@ -0,0 +1,22 @@
+package com.example.hive;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentNavigation extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
