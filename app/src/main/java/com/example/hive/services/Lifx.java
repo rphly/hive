e2af8dd45b46b7e8b73d26c308a416bb70e39112
Raphael Yee <raphaelyeejunkai@gmail.com>
2022-03-09 15:02:13 +0800

    fleshed out lifx apis

--- app/src/main/java/com/example/hive/services/Lifx.java
+++ app/src/main/java/com/example/hive/services/Lifx.java
@@ -1,6 +1,14 @@
 package com.example.hive.services;
 
-import static com.example.hive.Constants.LIFX_BASE_URL;
+import static com.example.hive.utils.Constants.DEFAULT_LIGHT_COLD_KELVIN;
+import static com.example.hive.utils.Constants.DEFAULT_LIGHT_WARM_KELVIN;
+import static com.example.hive.utils.Constants.LIFX_BASE_URL;
+import static com.example.hive.utils.Constants.LIFX_POST_STATE_DELTA_URL;
+import static com.example.hive.utils.Constants.LIFX_POST_TOGGLE_POWER_URL;
+import static com.example.hive.utils.Constants.LIFX_PUT_STATE_URL;
+import static com.example.hive.utils.Constants.Temperature;
+
+import android.annotation.SuppressLint;
 
 import com.google.gson.Gson;
 
@@ -11,8 +19,10 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class Lifx {
@@ -32,7 +42,7 @@ public class Lifx {
         this.getMainLight();
     }
 
-    private void getMainLight() throws IOException, JSONException {
+    private void getMainLight() throws JSONException {
         String url = String.format(LIFX_BASE_URL, "all");
         Request request = new Request.Builder()
                 .url(url)
@@ -47,13 +57,79 @@ public class Lifx {
                     this.mainLightId = (String) j.get("id");
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void toggleLight() {}
-    public void increaseBrightness() {}
-    public void decreaseBrightness() {}
-    public void toggleWarmOrColdLighting() {}
+    public void toggleLight() throws JSONException {
+        /* NOTE: Does not reset brightness of bulb by default */
+        String url = String.format(LIFX_POST_TOGGLE_POWER_URL, "id:" + this.mainLightId);
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"), "{}");
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Light successfully toggled.");
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void increaseBrightness() {
+        addBrightnessDelta(0.1);
+    }
+
+    public void decreaseBrightness() {
+        addBrightnessDelta(-0.1);
+    }
+
+    private void addBrightnessDelta(double delta) {
+        /*
+            Brightness is clipped at [0,1]. No error is thrown if delta results in brightness beyond range.
+            0 =< INITIAL_STATE + delta_1 + delta_2 + ... + delta_n <= 1
+            NOTE: this operation is additive.
+        */
+        String url = String.format(LIFX_POST_STATE_DELTA_URL, "id:" + this.mainLightId);
+        @SuppressLint("DefaultLocale")
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"), String.format("{\"brightness\": %.1f}", delta));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Light brightness successfully adjusted.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setLightTempColdOrWarm(Temperature temp) {
+        String url = String.format(LIFX_PUT_STATE_URL, "id:" + this.mainLightId);
+        int color = temp == Temperature.COLD ? DEFAULT_LIGHT_COLD_KELVIN : DEFAULT_LIGHT_WARM_KELVIN;
+        @SuppressLint("DefaultLocale")
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"), String.format("{\"color\": kelvin:%s}", color));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Light temp successfully toggled.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 class AuthorizationInterceptor implements Interceptor {
--- app/src/main/java/com/example/hive/utils/Constants.java
+++ app/src/main/java/com/example/hive/utils/Constants.java
@@ -5,16 +5,18 @@ public class Constants {
     public static final int DEFAULT_FOCUS_MODE = 0;
 
     // lights
-    public static final int DEFAULT_LIGHT_STATE = 0;
     public static final int DEFAULT_LIGHT_BRIGHTNESS_INTERVAL = 10;
-    public static final int DEFAULT_LIGHT_TEMP = 1600;
-
-    public static final int LIGHT_TEMP_COLD = 1600;
-    public static final int LIGHT_TEMP_WARM = 2200;
+    public static final int DEFAULT_LIGHT_COLD_KELVIN = 5000;
+    public static final int DEFAULT_LIGHT_WARM_KELVIN = 3200;
 
     // LIFX urls
     public static final String LIFX_BASE_URL = "https://api.lifx.com/v1/lights/%s/";
     public static final String LIFX_PUT_STATE_URL = LIFX_BASE_URL + "state";
-    public static final String LIFX_TOGGLE_POWER_URL = LIFX_BASE_URL + "toggle";
+    public static final String LIFX_POST_TOGGLE_POWER_URL = LIFX_BASE_URL + "toggle";
     public static final String LIFX_POST_STATE_DELTA_URL = LIFX_BASE_URL + "state/delta";
+
+    public static enum Temperature {
+        COLD,
+        WARM
+    }
 }
