e13e9010decfbc43479567b1d14f49206054e5ad
chris-lye <christopher.zijian@gmail.com>
2022-04-04 20:59:19 +0800

    Fix Desk manually

--- app/src/main/java/com/example/hive/models/Desk.java
+++ app/src/main/java/com/example/hive/models/Desk.java
@@ -1,9 +1,5 @@
 package com.example.hive.models;
 
-import android.annotation.SuppressLint;
-
-import androidx.annotation.NonNull;
-
 import com.example.hive.services.DeskService;
 import com.example.hive.services.Response;
 
@@ -11,19 +7,18 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Desk {
-    private final String id;
-    private final String currentUserId;
-    private final String lightToken;
-    private final String label;
-    private final Location location;
+    private String id, currentUserId, lightToken, label;
+    private Location location;
+    private int timeCheckedIn;
 
-    public Desk(String id, String currentUserId, String lightToken, String label, float x, float y) {
+    public Desk(String id, String currentUserId, String lightToken, String label, float x, float y, int timeCheckedIn) {
         this.id = id;
         this.currentUserId = currentUserId;
         this.lightToken = lightToken;
         this.label = label;
         this.location = new Location(x, y);
-    }
+        this.timeCheckedIn = timeCheckedIn;
+    };
 
     public boolean isAvailable() {
         return currentUserId == null;
@@ -31,6 +26,7 @@ public class Desk {
     public String getCurrentUserId() {
         return this.currentUserId;
     }
+    public int getTimeCheckedIn() { return this.timeCheckedIn; }
     public Location getLocation() {return location;}
     public String getLabel() {return this.label;}
     public void signIn(User user, Response handler) {
@@ -45,17 +41,18 @@ public class Desk {
         Map data = (Map) obj;
         String lightToken = String.valueOf(data.get("light_token"));
         String label = (String) data.get("label");
-        float x = Float.parseFloat(String.valueOf(data.get("location_x")));
-        float y = Float.parseFloat(String.valueOf(data.get("location_y")));
+        Float x = Float.valueOf(String.valueOf(data.get("location_x")));
+        Float y = Float.valueOf(String.valueOf(data.get("location_y")));
         String currentUserId = (String) data.get("current_user");
         String id = String.valueOf(data.get("id"));
-
-        return new Desk(id, currentUserId, lightToken, label, x, y);
+        int timeCheckedIn = Integer.valueOf(String.valueOf(data.get("time_checked_in")));;
+        Desk desk = new Desk(id, currentUserId, lightToken, label, x, y, timeCheckedIn);
+        return desk;
     }
 
     public static ArrayList<Desk> fromObjects(Object obj) {
         ArrayList res = (ArrayList) obj;
-        ArrayList<Desk> data = new ArrayList<>();
+        ArrayList data = new ArrayList<Desk>();
         for (Object o: res) {
             if (o != null) {
                 data.add(Desk.fromObject(o));
@@ -66,19 +63,22 @@ public class Desk {
 }
 
 class Location {
-    private final float x;
-    private final float y;
+    private float x;
+    private float y;
+
+    public Location() {};
 
     public Location(float x, float y) {
         this.x = x;
         this.y = y;
-    }
+    };
 
-    @SuppressLint("DefaultLocale")
-    @NonNull
     @Override
     public String toString() {
         return String.format("%f, %f", this.x, this.y);
     }
 
+    public void printLocation() {
+        System.out.println(this.toString());
+    }
 }
