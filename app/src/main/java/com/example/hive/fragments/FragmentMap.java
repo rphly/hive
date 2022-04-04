d742148fe47be0f8fb671f3915814331951197a7
chris-lye <christopher.zijian@gmail.com>
2022-04-04 21:46:19 +0800

    Copy over FragmentMap from Joyce+Chris branch

--- app/src/main/java/com/example/hive/fragments/FragmentMap.java
+++ app/src/main/java/com/example/hive/fragments/FragmentMap.java
@@ -1,28 +1,52 @@
 package com.example.hive.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.example.hive.models.Desk;
+import com.example.hive.services.DeskService;
 import androidx.fragment.app.Fragment;
 
 import com.example.hive.R;
+import com.example.hive.services.Response;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class FragmentMap extends Fragment {
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
+
+    public void makeButton(int x, int y, String deskID, RelativeLayout relLayout, int mapWidth, int mapHeight) {
+        //puts a button the specified relative layout
+        Button b1 = new Button(relLayout.getContext());
+        b1.setAlpha(1.0F);
+        b1.setText("Desk" + deskID);
+        //Set the button params, the position of it in its parent relative layout
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        buttonParams.leftMargin = (int) Math.round(x/100.0 * mapWidth); //map width is the width of the image, in pixels
+        buttonParams.topMargin = (int) Math.round(y/100.0 * mapWidth);
+        String location = "(" + x + "," + y + ")";
+        b1.setText(location);
+        relLayout.addView(b1, buttonParams); //add the button
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
@@ -61,22 +85,34 @@ public class FragmentMap extends Fragment {
         final int maxBottom = maxY;
 
         //Set up user buttons and the user map overlay
-        RelativeLayout mapFrame = (RelativeLayout) view.findViewById(R.id.usermap);
-        ViewGroup.LayoutParams mapLayoutParams = mapFrame.getLayoutParams();
-        mapLayoutParams.width = bitmapWidth;
-        mapLayoutParams.height = bitmapHeight;
-        mapFrame.setLayoutParams(mapLayoutParams);
+        RelativeLayout mapRelativeLayout = view.findViewById(R.id.usermap);
+        FrameLayout.LayoutParams mapLayoutParams1 = (FrameLayout.LayoutParams) mapRelativeLayout.getLayoutParams();
+        mapLayoutParams1.width = bitmapWidth;
+        mapLayoutParams1.height = bitmapHeight;
+
+        // https://stackoverflow.com/questions/18655940/linearlayoutlayoutparams-cannot-be-cast-to-android-widget-framelayoutlayoutpar
+        mapRelativeLayout.setLayoutParams(mapLayoutParams1);
+
+        //Get Desk
+        // onSuccess, will use makeButton to add in the user
+        String deskID2 = "2";
+        DeskService.getDeskById(deskID2, new Response() {
+            @Override
+            public void onSuccess(Object data) {
+                Map data2= (Map) data;
+                System.out.println(data2.toString());
+                int button_x = (int) (long) data2.get("location_x"); //idk why but apparently firebase gives long
+                int button_y = (int) (long) data2.get("location_y");
+                makeButton(button_x, button_y, deskID2, mapRelativeLayout, bitmapWidth, bitmapHeight);
+            }
+            @Override
+            public void onFailure() {
+                System.out.println("Failed to load: " + deskID2 );
+            }
+        });
 
         // test button
-        Button b1 = new Button(mapFrame.getContext());
-        b1.setAlpha(1.0F);
-        b1.setText("BtnTest");
 
-        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        buttonParams.leftMargin = -bitmapWidth/4;
-        buttonParams.topMargin = 0;
-
-        mapFrame.addView(b1, buttonParams);
 
         ImageView_BitmapView.setOnTouchListener(new View.OnTouchListener()
         {
@@ -136,7 +172,7 @@ public class FragmentMap extends Fragment {
                         }
 
                         // scrolling to top of image (pic moving to the bottom)
-                        else if (currentY > downY)
+                        if (currentY > downY)
                         {
                             if (totalY == maxTop)
                             {
@@ -171,7 +207,7 @@ public class FragmentMap extends Fragment {
                             }
                         }
 
-                        mapFrame.scrollBy(scrollByX, scrollByY);
+                        mapRelativeLayout.scrollBy(scrollByX, scrollByY);
                         ImageView_BitmapView.scrollBy(scrollByX, scrollByY);
                         downX = currentX;
                         downY = currentY;
