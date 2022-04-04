9191bfdc8dd5c61fda4a8e6ec2e1c316154c1211
joyceeqq <joyce041144@gmail.com>
2022-04-04 19:01:01 +0800

    Merge pull request #7 from rphly/revert-5-meihui-patch-1-2
    Revert "Add pics"

--- app/src/main/java/com/example/hive/models/User.java
+++ app/src/main/java/com/example/hive/models/User.java
@@ -2,13 +2,15 @@ package com.example.hive.models;
 
 import com.example.hive.utils.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class User {
-    private String firstName, lastName, email, id, profilePic;
-    private Constants.Status status;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String id;
+    private final Constants.Status status;
+
     public User(String id, String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -17,23 +19,14 @@ public class User {
         this.status = Constants.Status.AVAILABLE;
     }
 
-    public User(String id, String firstName, String lastName, String email, String profilePic, Constants.Status status) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.id = id;
-        this.status = status;
-        this.profilePic = profilePic;
-    }
-
     public Constants.Status getStatus() {return this.status;}
     public String getEmail() {return this.email;}
     public String getFirstName() {return this.firstName;}
     public String getLastName() {return this.lastName;}
     public String getFullName() {return this.firstName + " " + this.lastName;}
     public String getId() {return this.id;}
-    public String getProfilePicUri() {
-        return this.profilePic;
+    public void getCurrentDesk() {
+        // get id from firebase
     }
 
     public static User fromObject(Object obj) {
@@ -42,22 +35,7 @@ public class User {
         String lastName = (String) data.get("lastName");
         String email = (String) data.get("email");
         String id = String.valueOf(data.get("id"));
-        Constants.Status status = Constants.Status.valueOf((String) data.get("status"));
-        String profilePic = (String) data.get("profilePic");
 
-        User user = new User(id, firstName, lastName, email, profilePic, status);
-        return user;
-    }
-
-    public static ArrayList<User> fromObjects(Object obj) {
-        Map raw = (Map) obj;
-        List<Object> list = new ArrayList<Object>(raw.values());
-        ArrayList data = new ArrayList<User>();
-        for (Object o: list) {
-            if (o != null) {
-                data.add(User.fromObject(o));
-            }
-        }
-        return data;
+        return new User(id, firstName, lastName, email);
     }
 }
