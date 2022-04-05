70f04a08cbfbaf464fe2ca41e78ae5b84794ee79
Raphael Yee <raphaelyeejunkai@gmail.com>
2022-04-05 17:24:54 +0800

    gvebjkhwbjkevb

--- app/src/main/java/com/example/hive/services/UserService.java
+++ app/src/main/java/com/example/hive/services/UserService.java
@@ -1,7 +1,10 @@
 package com.example.hive.services;
 
+import com.example.hive.utils.Constants;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class UserService extends BaseService {
     final static DatabaseReference collection = FirebaseDatabase.getInstance().getReference().child("Users");
@@ -12,6 +15,16 @@ public class UserService extends BaseService {
 
     public static void getAllUsers(Response handler) {
         getFromFirebase(collection, handler);
+    }
+
+    public static void setUserStatus(String id, Constants.Status status, Response handler) {
+        HashMap toUpdate = new HashMap();
+        toUpdate.put("status", status);
+        try {
+            updateFirebase(collection.child(id), toUpdate, handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
